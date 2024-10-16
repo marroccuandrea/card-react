be1d6ce189a7384e3a9b0a934572d442775e3631
marroccuandrea <marroccuandrea@yahoo.it>
2024-10-16 18:29:34 +0200

    dinamicità fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ const primaCard = {
   nome: "Asciugamano",
   img: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f14712.jpg",
   prezzo: 2.16,
+  prezzoFinale: 1.90,
+  utente: "Leo787",
 }
 const secondaCard = {
   nome: "Cuccia Gatti",
   img: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f20099.jpg",
   prezzo: 12.16,
+  prezzoFinale: 11,
+  utente: "Bamby000",
 }
 
 function App() {
@@ -16,8 +20,8 @@ function App() {
     <div className="App">
       <h1>La mia prima Card</h1>
       {/* <Product nome="mario" zona="rossa" /> */}
-      <Product nome={primaCard.nome} prezzo={primaCard.prezzo} img={primaCard.img}/>
-      <Product nome={secondaCard.nome} prezzo={secondaCard.prezzo} img={secondaCard.img}/>
+      <Product nome={primaCard.nome} prezzo={primaCard.prezzo} prezzoFinale={primaCard.prezzoFinale} img={primaCard.img} utente={primaCard.utente}/>
+      <Product nome={secondaCard.nome} prezzo={secondaCard.prezzo} prezzoFinale={secondaCard.prezzoFinale} img={secondaCard.img} utente={secondaCard.utente}/>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // const prezzo = '10'
-const alt = 'valore'
+// const alt = 'valore'
 // const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f20099.jpg"
 
 const Product = (props) => {
@@ -9,14 +9,14 @@ const Product = (props) => {
   return (
     <article>
         <div className="card">
-            <img src={props.img} alt={`${alt}`} />
+            <img src={props.img} alt={`${props.nome}`} />
             <h6>{props.nome}</h6>
             <hr />
-            <p>2,16 €</p>
+            <p>{props.prezzo} €</p>
             <p>{props.nome}</p>
-            <p style={{textTransform: 'uppercase'}}>bamby0000</p>
+            <p style={{textTransform: 'uppercase'}}>{props.utente}</p>
             <p className="card-time">RIAPRE PRESTO</p>
-          <button>{props.prezzo} €</button>
+          <button>{props.prezzoFinale} €</button>
         </div>
         
     </article>
