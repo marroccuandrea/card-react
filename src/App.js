44ed2b8ea8b754813c0423e829b92d612b1231f7
marroccuandrea <marroccuandrea@yahoo.it>
2024-10-16 18:21:11 +0200

    Card rese dinamiche tramite props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Card from "./Card";
+import Product from "./Product";
 
+const primaCard = {
+  nome: "Asciugamano",
+  img: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f14712.jpg",
+  prezzo: 2.16,
+}
+const secondaCard = {
+  nome: "Cuccia Gatti",
+  img: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f20099.jpg",
+  prezzo: 12.16,
+}
 
 function App() {
   return (
     <div className="App">
       <h1>La mia prima Card</h1>
-      <Card />
+      {/* <Product nome="mario" zona="rossa" /> */}
+      <Product nome={primaCard.nome} prezzo={primaCard.prezzo} img={primaCard.img}/>
+      <Product nome={secondaCard.nome} prezzo={secondaCard.prezzo} img={secondaCard.img}/>
     </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// const prezzo = '10'
+const alt = 'valore'
+// const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f20099.jpg"
+
+const Product = (props) => {
+//  console.log(props.nome);
+ 
+  return (
+    <article>
+        <div className="card">
+            <img src={props.img} alt={`${alt}`} />
+            <h6>{props.nome}</h6>
+            <hr />
+            <p>2,16 €</p>
+            <p>{props.nome}</p>
+            <p style={{textTransform: 'uppercase'}}>bamby0000</p>
+            <p className="card-time">RIAPRE PRESTO</p>
+          <button>{props.prezzo} €</button>
+        </div>
+        
+    </article>
+  )
+}
+
+export default Product
